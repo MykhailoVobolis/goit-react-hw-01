ca8a2878b2da425a1758af48cf94daca6d7bfd0c
MykhailoVobolis <mvobolis@gmail.com>
2024-03-27 20:56:46 +0200

    hw-01 fix comments

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,13 @@
+import "./App.css";
+
+import Profile from "../Profile/Profile";
+import FriendList from "../FriendList/FriendList";
+import TransactionHistory from "../TransactionHistory/TransactionHistory";
+
+import userData from "../../userData.json";
+import friends from "../../friends.json";
+import transactions from "../../transactions.json";
+
 export default function App() {
   return (
     <>
