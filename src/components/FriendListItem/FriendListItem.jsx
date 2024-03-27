8b3950e687a0cfa3f6f06670ec0e9e52216b538a
MykhailoVobolis <mvobolis@gmail.com>
2024-03-27 22:07:32 +0200

    hw-01 fix test-01

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,11 +1,1 @@
 import css from "./FriendListItem.module.css";
-
-export default function FriendListItem({ friends: { avatar, name, isOnline } }) {
-  return (
-    <div className={css.friend}>
-      <img className={css.friendAvatar} src={avatar} alt="Avatar" width="48" />
-      <p className={css.friendName}>{name}</p>
-      {isOnline ? <p className={css.isOnline}>Online</p> : <p className={css.isOffline}>Offline</p>}
-    </div>
-  );
-}
--- src/components/transactionHistory/TransactionHistory.jsx
+++ src/components/transactionHistory/TransactionHistory.jsx
@@ -1,24 +1,3 @@
 import TransactionItem from "./TransactionItem";
 
 import css from "./TransactionHistory.module.css";
-
-export default function TransactionHistory({ items }) {
-  return (
-    <table className={css.transactionTab}>
-      <thead>
-        <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
-        </tr>
-      </thead>
-      <tbody>
-        {items.map((transaction) => (
-          <tr key={transaction.id}>
-            <TransactionItem transaction={transaction} />
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-}
